29b48dc91602eea3fb9479b516924a7e195f3b7a
UnnatiPulla <upulla@purdue.edu>
2018-05-14 19:53:52 -0400

    Homework1 - bonus - add multiple headings and change the second heading when button clicked

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const button = document.querySelector("button.greeting")
-const heading = document.querySelector("h1.heading1")
+const heading2 = document.querySelector("h1.heading2")
 function changeText(){
-    heading.textContent='Button has been Clicked'; 
+    heading2.textContent='Button has been Clicked to change heading 2'; 
 }
 button.addEventListener("click", changeText)
