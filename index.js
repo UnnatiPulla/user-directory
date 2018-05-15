2164861feb66c50446802e185546c29c7f1bb671
UnnatiPulla <upulla@purdue.edu>
2018-05-14 21:02:01 -0400

    Homework1 - extra bonus - simpler than the previous commit

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const button = document.querySelector("button.greeting")
 const heading2 = document.querySelector("h1.heading2")
 const heading1 = document.querySelector("h1.heading1");
+
 function changeText(){
     heading2.textContent='Button has been Clicked to change heading 2'; 
 }
 
-function textInput(form){
-    const input = form.userInput.value;
-    heading1.textContent=input;
+function textInput(){
+    const textBox = document.querySelector("input.userInput").value
+    heading1.textContent=textBox;
 }
 
 button.addEventListener("click", changeText)
