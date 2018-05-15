6da96102105c11563a6492a9863b8200665961b6
UnnatiPulla <upulla@purdue.edu>
2018-05-14 20:51:18 -0400

    Homework1 - extra bonus - add a form and change heading to user input

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const button = document.querySelector("button.greeting")
 const heading2 = document.querySelector("h1.heading2")
+const heading1 = document.querySelector("h1.heading1");
 function changeText(){
     heading2.textContent='Button has been Clicked to change heading 2'; 
 }
+
+function textInput(form){
+    const input = form.userInput.value;
+    heading1.textContent=input;
+}
+
 button.addEventListener("click", changeText)
