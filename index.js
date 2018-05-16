17d2d13cf6f609a7b118268e40cb9358b39d8438
UnnatiPulla <upulla@purdue.edu>
2018-05-16 15:22:43 -0400

    add slabo font using google fonts

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 
 const form = document.querySelector("#userForm");
+const users = document.querySelector("#users");  
 
 const handleSubmit = function(ev){
     ev.preventDefault()
+    const f = ev.target;  
     
-    const f = ev.target;
-    const userName = f.userName.value;
-    const age = f.age.value;
-    const favoritecolor = f.favoritecolor.value;
+    
+        const userName = f.userName.value;
+        const age = f.age.value;
+        const favoritecolor = f.favoritecolor.value;
     
     users.appendChild(renderList(userName, age, favoritecolor));
 
@@ -16,37 +18,38 @@ const handleSubmit = function(ev){
 }
 
 function renderColor(favoritecolor){
-    const colorDiv = document.createElement('div');
-    colorDiv.style.backgroundColor = favoritecolor;
-    colorDiv.style.width = '6rem'
+    const colorDiv = document.createElement('div'); 
+    colorDiv.style.backgroundColor = favoritecolor; 
+
+    colorDiv.style.width = '6rem' 
     colorDiv.style.height = '3rem'
-    return colorDiv;
+    return colorDiv;    
 }
 
-function renderListItem(){
-    const nameItem = document.createElement('li');
-    const ageItem = document.createElement('li');
+function renderListItem(){  
+    const nameItem = document.createElement('li'); 
+    const ageItem = document.createElement('li'); 
     const colorItem = document.createElement('li');
-    nameItem.textContent = `Name: ${arguments[0]}`
-    ageItem.textContent = `Age: ${arguments[1]}`
+    nameItem.textContent = `Name: ${arguments[0]}`; 
+    ageItem.textContent = `Age: ${arguments[1]}`; 
     colorItem.textContent = `Favorite Color: `;
-    colorItem.appendChild(renderColor(arguments[2]));
-
-    return [nameItem, ageItem, colorItem];
-
+    colorItem.appendChild(renderColor(arguments[2]));  
+    
+    return [nameItem, ageItem, colorItem]; 
+                                                                            
 }
 
-function renderList(){
+
+
+function renderList(data){
     const list = document.createElement('ul');
     const items = renderListItem(arguments[0], arguments[1], arguments[2]);
+
+    list.appendChild(items[0]);  
+    list.appendChild(items[1]); 
+    list.appendChild(items[2]); 
     
-    list.appendChild(items[0]);
-    list.appendChild(items[1]);
-    list.appendChild(items[2]);
-    
-    const users = document.querySelector("#users");
     return list;
 }
-
 
 form.addEventListener("submit", handleSubmit)
